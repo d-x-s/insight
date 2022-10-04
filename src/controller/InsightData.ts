99d46225882db565293441246485288bbb78dfc7
Wesley <wesleyhchoi@gmail.com>
2022-10-03 21:34:13 -0700

    created constructor for class

--- src/controller/InsightData.ts
+++ src/controller/InsightData.ts
@@ -3,8 +3,8 @@ import {InsightDatasetKind} from "./IInsightFacade";
 export default class InsightData {
 
 	public constructor(id: string, kind: InsightDatasetKind) {
-
+		let content: string;
+		let type: InsightDatasetKind;
 	}
-
 
 }
