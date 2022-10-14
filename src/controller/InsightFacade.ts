652f7bc18e5a5c06eac3c06456a577dde59a54c2
UBC Student <davissong01@gmail.com>
2022-10-14 13:30:45 -0700

    Setup all method stubs

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,15 +1,16 @@
-import {IInsightFacade,
+import {
+	IInsightFacade,
 	InsightDataset,
 	InsightDatasetKind,
 	InsightError,
 	InsightResult,
-	ResultTooLargeError} from "./IInsightFacade";
+	ResultTooLargeError,
+} from "./IInsightFacade";
 import Utility from "../Utility";
 import ValidateQueryHelper from "./ValidateQueryHelper";
 import JSZip from "jszip";
 import {Dataset} from "./Dataset";
 import {SectionsData} from "./SectionsData";
-import {KeyObject} from "crypto";
 import PerformQueryHelper from "./PerformQueryHelper";
 
 /**
@@ -18,7 +19,6 @@ import PerformQueryHelper from "./PerformQueryHelper";
  *
  */
 export default class InsightFacade implements IInsightFacade {
-
 	// locally instantiated model containing Datasets
 	public internalModel: Map<string, Dataset>;
 
@@ -59,9 +59,10 @@ export default class InsightFacade implements IInsightFacade {
 	private addDatasetToModel(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		let zipped: JSZip = new JSZip();
 
-		return new Promise<string[]> ((resolve, reject) => {
+		return new Promise<string[]>((resolve, reject) => {
 			let dataToProcess: Array<Promise<string>> = [];
-			zipped.loadAsync(content, {base64: true})
+			zipped
+				.loadAsync(content, {base64: true})
 				.then((file) => {
 					let fileFolder = file.folder("courses");
 					if (fileFolder == null || fileFolder === undefined) {
@@ -78,24 +79,28 @@ export default class InsightFacade implements IInsightFacade {
 						dataToProcess.push(currCourse.async("text"));
 					});
 					return dataToProcess;
-				}).then((value: Array<Promise<string>> | InsightError) => {
+				})
+				.then((value: Array<Promise<string>> | InsightError) => {
 					if (value instanceof InsightError) {
 						return new InsightError("ERROR: InsightError caught ");
 					}
-					Promise.all(value).then(() => {
-						let pushDataset: Promise<SectionsData[]> = this.pushToDataset(dataToProcess, content);
-						return pushDataset;
-					}).then((pushDataset) => {
-						let newDataset: Dataset = {
-							id: id,
-							sectionData: pushDataset,
-							kind: kind
-						};
-						this.internalModel.set(id, newDataset);
-						let updateKeysAfterAdd: string[] = Array.from(this.internalModel.keys());
-						resolve(updateKeysAfterAdd);
-					});
-				}).catch((err) => {
+					Promise.all(value)
+						.then(() => {
+							let pushDataset: Promise<SectionsData[]> = this.pushToDataset(dataToProcess, content);
+							return pushDataset;
+						})
+						.then((pushDataset) => {
+							let newDataset: Dataset = {
+								id: id,
+								sectionData: pushDataset,
+								kind: kind,
+							};
+							this.internalModel.set(id, newDataset);
+							let updateKeysAfterAdd: string[] = Array.from(this.internalModel.keys());
+							resolve(updateKeysAfterAdd);
+						});
+				})
+				.catch((err) => {
 					reject(new InsightError("Failed to parse" + err));
 				});
 		});
@@ -111,28 +116,27 @@ export default class InsightFacade implements IInsightFacade {
 				let dataFromJSON = data["result"];
 				for (let dataElement in dataFromJSON) {
 					pushDataset.push(dataElement);
-				};
+				}
 			}
 			return Promise.resolve(pushDataset);
 		} catch {
 			return Promise.reject(new InsightError("ERROR: could not push to dataset"));
-		};
+		}
 	}
 
 	/*
-    * addDataset(id: string, content: string, kind: InsightDatasetKind):
-    * Promise<string[]> adds a dataset to the internal model,
-    * providing the id of the dataset, the string of the content
-    * of the dataset, and the kind of the dataset.
-    *
-    * For this checkpoint the dataset kind will be sections,
-    * and the rooms kind is invalid. A valid id is an id string
-    * is defined in the EBNF. Additionally, an id that is only
-    * whitespace is invalid.
-    * Any invalid inputs should be rejected.
-    * */
+	 * addDataset(id: string, content: string, kind: InsightDatasetKind):
+	 * Promise<string[]> adds a dataset to the internal model,
+	 * providing the id of the dataset, the string of the content
+	 * of the dataset, and the kind of the dataset.
+	 *
+	 * For this checkpoint the dataset kind will be sections,
+	 * and the rooms kind is invalid. A valid id is an id string
+	 * is defined in the EBNF. Additionally, an id that is only
+	 * whitespace is invalid.
+	 * Any invalid inputs should be rejected.
+	 * */
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-
 		// check param @id for validity
 		if (!this.checkId(id)) {
 			return Promise.reject(new InsightError("Error in addDataset: id is invalid"));
@@ -158,19 +162,17 @@ export default class InsightFacade implements IInsightFacade {
 		// return Promise.reject(new InsightError("Not implemented"));
 		// Assuming all inputs are valid, we can push this to the internal model.
 		return Promise.resolve(this.addDatasetToModel(id, content, kind));
-
 	}
 
 	/*
-    * removeDataset(id: string): Promise<string> removes a
-    * dataset from the internal model, given the id. A valid id
-    * is as idstring is defined in the EBNF. As above, an id
-    * that is only whitespace is invalid. In addition, removing
-    * a nonexistent id should be rejected.
-    */
+	 * removeDataset(id: string): Promise<string> removes a
+	 * dataset from the internal model, given the id. A valid id
+	 * is as idstring is defined in the EBNF. As above, an id
+	 * that is only whitespace is invalid. In addition, removing
+	 * a nonexistent id should be rejected.
+	 */
 	public removeDataset(id: string): Promise<string> {
-
-		return new Promise<string> ((resolve, reject) => {
+		return new Promise<string>((resolve, reject) => {
 			// check param @id for validity (EBNF standards) and whitespace verification
 			if (!this.checkId(id)) {
 				reject(new InsightError("Error in addDataset: id is invalid"));
@@ -193,7 +195,6 @@ export default class InsightFacade implements IInsightFacade {
     then it should reject with a ResultTooLargeError.
     */
 	public performQuery(query: unknown): Promise<InsightResult[]> {
-
 		return new Promise((resolve, reject) => {
 			let validator = new ValidateQueryHelper();
 			let performer = new PerformQueryHelper();
@@ -216,7 +217,8 @@ export default class InsightFacade implements IInsightFacade {
 
 			let result: any[];
 			try {
-				result = performer.processFilter(query, this.internalModel.get(id));
+				// TODO: add a check if the WHERE clause has 0 keys
+				result = performer.processQuery(query, this.internalModel.get(id));
 				result = performer.processOptions();
 			} catch {
 				return reject(new InsightError("performQuery::Error while querying"));
@@ -254,5 +256,4 @@ export default class InsightFacade implements IInsightFacade {
 			resolve(listDatasetsFromLocal);
 		});
 	}
-
 }
--- src/controller/PerformQueryHelper.ts
+++ src/controller/PerformQueryHelper.ts
@@ -1,21 +1,71 @@
 import Utility from "../Utility";
 import {Dataset} from "./Dataset";
+import {SectionsData} from "./SectionsData";
 
 export default class PerformQueryHelper {
+	protected kind: any;
+
 	constructor() {
 		Utility.log("initializing PerformQueryHelper", "trace");
 	}
 
-	public processFilter(query: any, data: Dataset | undefined): any[] {
-
-		if (data === undefined) {
+	public processQuery(query: any, dataset: Dataset | undefined): any[] {
+		if (dataset === undefined) {
 			throw Error("The dataset being queried on is undefined");
 		}
-
-		return [];
+		this.kind = dataset.kind;
+		return this.filterQuery(query["WHERE"], dataset.sectionData, this.kind);
+		// return this.where(query["WHERE"], dataset.sectionData, this.kind);
 	}
 
 	public processOptions(): any[] {
 		return [];
 	}
+
+	private filterQuery(query: any, sections: SectionsData[], kind: any): any[] {
+		return sections.filter((section) => {
+			return this.where(query, section, kind);
+		});
+	}
+
+	private where(query: any, section: SectionsData, kind: any): boolean {
+		let key = Object.keys(query)[0];
+
+		switch (key) {
+			case "AND":
+				return this.and(query, section, kind);
+			case "OR":
+				return this.or(query, section, kind);
+			case "LT":
+			case "GT":
+			case "EQ":
+				return this.mComparator(query, section, kind, key);
+			case "IS":
+				return this.sComparator(query, section, kind, key);
+			case "NOT":
+				return this.not(query, section, kind);
+			default:
+				throw new Error("Malformed operator key name");
+		}
+	}
+	private and(query: any, section: SectionsData, kind: any): boolean {
+		throw new Error("Method not implemented.");
+	}
+
+	private or(query: any, section: SectionsData, kind: any): boolean {
+		throw new Error("Method not implemented.");
+	}
+
+	private mComparator(query: any, section: SectionsData, kind: any, key: string): boolean {
+		throw new Error("Method not implemented.");
+	}
+
+	private sComparator(query: any, section: SectionsData, kind: any, key: string): boolean {
+		throw new Error("Method not implemented.");
+	}
+
+	private not(query: any, section: SectionsData, kind: any): boolean {
+		throw new Error("Method not implemented.");
+	}
+
 }
