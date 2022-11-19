7372bbdfba4c8a43a22c4f89a35d9a1e50b1fa06
UBC Student <davissong01@gmail.com>
2022-11-18 16:53:49 -0800

    ESLint

--- src/controller/dataset-rooms/RoomsHelper.ts
+++ src/controller/dataset-rooms/RoomsHelper.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines */
 import {InsightDatasetKind, InsightError, InsightResult} from "../IInsightFacade";
 import {IRoomDataset} from "./IRoomDataset";
 import JSZip from "jszip";
@@ -36,15 +35,13 @@ export default class RoomsHelper {
 		model: Map<string, IRoomDataset>
 	): Promise<string[]> {
 		let zipped: JSZip = new JSZip();
-		// console.log("1");
 		return new Promise<string[]> ((resolve, reject) => {
 			zipped.loadAsync(content, {base64: true})
 				.then((loadedZipFiles) => {
-					// console.log("point 1 reached loaded zipfiles" + loadedZipFiles);
 					return this.handleRoomProcessing(loadedZipFiles);
 				}).then((result) => {
 					let processedResults = this.processResult(result);
-					// console.log(processedResults);
+					console.log(processedResults);
 					resolve(this.setDataToModelAndDisk(id, processedResults, kind, content, model));
 				})
 				.catch((err) => {
@@ -54,18 +51,14 @@ export default class RoomsHelper {
 	}
 
 	public processResult(result: any[]) {
-		// console.log(result);
 		let retArray: any = [];
 
 		result.forEach((room) => {
-			// console.log(room);
 			let newRoom: any  = {};
 			let objKeyArray = Object.keys(room);
-			// console.log(objKeyArray);
 
 			objKeyArray.forEach((key) => {
 				let newKey = this.datasetID + "_" + key;
-				// console.log(newKey);
 				newRoom[newKey] = room[key];
 			});
 			retArray.push(newRoom);
@@ -80,43 +73,24 @@ export default class RoomsHelper {
 			if (indexHtmFile == null) {
 				return new InsightError("file was null");
 			}
-			// console.log("point 2 reached");
 			parsedZip = indexHtmFile.async("string").then((indexHtmFileContent) => {
-				// console.log(indexHtmFileContent);
-				// console.log("point 2.5 reached");
 				let parsedFileContentDocument = parse(indexHtmFileContent);
-				// console.log(parsedFileContentDocument);
-				// console.log("point 2.6 reached");
-				// this.indexHtmBuildingHelper(parsedFileContent);
-				// console.log(this.buildingListObject);
 				for (let contentNode of parsedFileContentDocument.childNodes) {
 					let currNodeName = contentNode.nodeName;
 					if (currNodeName === "html") {
-						// console.log("line 69");
-						// console.log(contentNode);
 						this.indexHtmBuildingHelper(contentNode);
-						// console.log(this.buildingListObject);
-						// console.log(this.internalBuildings);
 					}
 				}
-				// console.log(this.buildingListObject);
-				// console.log("good");
 				return;
 			}).catch((err) => {
 				reject(new InsightError("unable to async" + err));
 			});
 
 			console.log("point 2.9 reached");
-			// console.log(this.buildingListObject);
 			Promise.all([parsedZip]).then(() => {
 				console.log("point 3 reached");
-				// NOTE: SOMETHING IS FILLING THIS UP I DONT KNOW WHAT?
-				// console.log(this.internalRooms);
 				this.findLocation.processLatAndLong(this.buildingListObject)
 					.then(() => {
-						// console.log(this.buildingListObject);
-						// console.log("point 4 reached");
-						// console.log(zipped);
 						resolve(this.processRooms(zipped));
 					}).catch((err) => {
 						reject(new InsightError("ERROR: unable to process lat" + err));
@@ -127,45 +101,30 @@ export default class RoomsHelper {
 	}
 
 	public processRooms(zipped: any): Promise<IRoomData[]> {
-		// console.log(this.internalRooms);
 		return new Promise<IRoomData[]> ((resolve, reject) => {
-			// console.log("point 5");
 			let dataToPush: Array<Promise<IRoomData>> = [];
 			let fileFolder = zipped.folder("campus/discover/buildings-and-classrooms");
-
 			if (fileFolder == null) {
 				return new InsightError("InsightError: null file folder, could not load");
 			}
-			// console.log("point 5.5");
-
 			fileFolder.forEach((buildingPath: any, file: any) => {
-				// console.log("file", file);
 				dataToPush.push(file.async("string")
 					.then((result: any) => {
-						// console.log("6.1 reached");
-						// console.log("buidlingPath is " + buildingPath);
 						let buildingPathNoHTM = buildingPath.split(".")[0];
-						// console.log("buidlingPathNoHTM is " + buildingPathNoHTM);
-						// console.log(this.buildingListObject[buildingPathNoHTM]);
 						this.processRoomsHelper(this.buildingListObject[buildingPathNoHTM], parse(result));
 					}).catch((err: any) => {
 						reject(new InsightError("Unable to process room" + err));
 					}));
 			});
-
 			Promise.all(dataToPush).then(() => {
-				// console.log("all");
 				resolve(this.internalIndex);
 			});
 		});
 	}
 
-
 	public processRoomsHelper(roomToProcess: any, res: any) {
-		// console.log("point 6.5");
 		for (let resNode of res.childNodes) {
 			if (resNode.nodeName === "html") {
-				// console.log("point7");
 				return this.addProcessedRooms(roomToProcess, resNode);
 			}
 		}
@@ -186,13 +145,11 @@ export default class RoomsHelper {
 		}
 	}
 
-	// NOTE: THIS IS GOOD< RETURNING A LIST OF ROOMS
 	public populateIRoomData(roomToAdd: any, node: any) {
 		if (node === null) {
 			return;
 		}
 		let newRoom = Object.assign({}, roomToAdd);
-
 		for (let param of node.childNodes) {
 			if (param.nodeName === "td") {
 				let currAttrs = param.attrs[0]["value"];
@@ -224,30 +181,22 @@ export default class RoomsHelper {
 				}
 			}
 		}
-		// console.log(newRoom);
 		this.internalIndex.push(newRoom);
 	}
 
 
 	public indexHtmBuildingHelper(contentNode: any) {
-		// if (contentNode == null) {
-		// 	return;
-		// }
-		// console.log(this.buildingListObject);
 		if (contentNode != null) {
-			// let newRoom: IRoomData = {} as IRoomData;
 			for (let contentNodeChild of contentNode.childNodes) {
 				let currName = contentNodeChild.nodeName;
 				if (currName === "tbody") {
 					for (let tr of contentNodeChild.childNodes) {
 						if (tr.nodeName === "tr") {
-							// this.convertIntoBuilding(tr, newRoom);
 							this.convertIntoBuilding(tr);
 						}
 					}
 				} else if (currName === "body" || currName === "div" || currName === "table"
 					|| currName === "section") {
-						// console.log("enter line 221");
 					this.indexHtmBuildingHelper(contentNodeChild);
 				}
 			}
@@ -257,28 +206,12 @@ export default class RoomsHelper {
 	private convertIntoBuilding(child: any) {
 		let newRoom: IRoomData = {} as IRoomData;
 		for (let childNode of child.childNodes) {
-			// console.log(childNode);
-			// console.log("inside Convert Into building");
 			if (childNode.nodeName === "td") {
 				let tempAttrs = childNode.attrs[0]["value"];
 				if (tempAttrs === "views-field views-field-title") {
-
-					// console.log("line 224");
-
 					for (let searchName of childNode.childNodes) {
-
-						// console.log("line 228");
-
 						if (searchName.nodeName === "a") {
-
-							// console.log("line 232");
-							// console.log(searchName);
-							// console.log(searchName.attrs[1]["value"]);
-
 							if(searchName.attrs[1]["value"] === "Building Details and Map") {
-
-								// console.log("line 236");
-
 								for (let searchNameChild of searchName.childNodes) {
 									if (searchNameChild.nodeName === "#text") {
 										let text = searchNameChild.value;
@@ -286,17 +219,6 @@ export default class RoomsHelper {
 										newRoom.fullname = text;
 									}
 								}
-
-								// for (let searchNameChild of searchName.nodeName) {
-
-								// 	console.log("line 242");
-								// 	console.log(searchNameChild);
-
-								// 	if (searchNameChild.nodeName === "#text") {
-								// 		console.log(searchNameChild.value);
-								// 		newRoom.fullname = (searchNameChild).value as string;
-								// 	}
-								// }
 							}
 						}
 					}
@@ -307,18 +229,13 @@ export default class RoomsHelper {
 				}
 			}
 		}
-		// console.log(this.buildingListObject);
 		if (newRoom.shortname != null) {
-			// console.log(newRoom.shortname);
-			// console.log(this.buildingListObject);
 			this.internalBuildings.push(newRoom);
-			// console.log(newRoom);
 			this.buildingListObject[newRoom.shortname] = newRoom;
 		};
 	}
 
 	private trimText(paramToTrim: any) {
-		// console.log("param", paramToTrim);
 		for (let paramChild of paramToTrim.childNodes) {
 			if (paramChild.nodeName === "#text") {
 				let trimmed = (paramChild).value.replace("\n", "").trim();
@@ -328,7 +245,6 @@ export default class RoomsHelper {
 		return "";
 	}
 
-	// HELPER: Sets data to internal model and to disk
 	private setDataToModelAndDisk(
 		id: string,
 		convertedRooms: IRoomData[],
@@ -357,5 +273,4 @@ export default class RoomsHelper {
 			return resolve(updateKeysAfterAdd);
 		});
 	}
-
 }
