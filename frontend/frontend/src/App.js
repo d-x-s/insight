007ffdba4c6126524fdf5b760414e26f57f4c518
wesleychoi <wesleyhchoi@gmail.com>
2022-11-28 13:28:51 -0800

    front end skeleton logic

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+// import {handleSubmission} from "./HandleSubmit";
+import {useRef} from "react";
 
 function App() {
+	const firstRoomInputField = useRef(null);
+	const secondRoomInputField = useRef(null);
+	const instructorInputField = useRef(null);
+
+	const handleRoomSubmission = () => {
+		let firstRoomInput = firstRoomInputField.current.value;
+		let secondRoomInput = secondRoomInputField.current.value;
+
+		// let firstRoomQuery = createRoomQuery(firstRoomInput);
+		// let secondRoomQuery = createRoomQuery(secondRoomInput);
+
+		// call
+
+
+
+
+	}
+
+	const handleInstructorSubmission = () => {
+		let instructorInput = instructorInputField.current.value;
+	}
+
+
   return (
     <div className="App">
       <header className="App-header">
 	  	<p>
 			Input rooms here:
 		</p>
-		<input className="first-room-input" />
-	  	<input className="second-room-input" />
+		<input id="first-room-input" ref={firstRoomInputField} />
+	  	<input className="second-room-input" ref={secondRoomInputField} />
 		  <br></br>
-	  	<button id="submit-rooms">Submit Names</button>
+	  	<button id="submit-rooms" onClick={handleRoomSubmission}>Submit Names</button>
 
 	  	<p>
 			Input instructor here:
 		</p>
-	  	<input className="instructor-input" />
+	  	<input className="instructor-input" ref={instructorInputField} />
 		  <br></br>
-	  	<button id="submit-instructors">Submit Instructor</button>
+	  	<button id="submit-instructors" onClick={handleInstructorSubmission}>Submit Instructor</button>
 
       </header>
     </div>
